18b81627ba9295e0edcf4f83d2d6a9e12327f8d1
sponbob-pat <ixdos1111@gmail.com>
2024-04-04 09:01:29 +0900

    chapter 4 피자 가게 프레임워크 만들기

---
+++ chapter4-factory/NYPizzaStore.java
@@ -0,0 +1,5 @@
+public class NYPizzaStore extends PizzaStore{
+
+    
+
+}
---
+++ chapter4-factory/PizzaStore.java
@@ -0,0 +1,23 @@
+
+public abstract class PizzaStore {
+
+    Pizza orderPizza(String type) { 
+        
+        Pizza pizza; 
+
+        pizza = createPizza(type);
+
+        pizza.prepare(); 
+        pizza.bake(); 
+        pizza.cut(); 
+        pizza.box(); 
+        
+        return pizza; 
+
+    }
+
+    abstract Pizza createPizza(String type); 
+
+}
+
+
